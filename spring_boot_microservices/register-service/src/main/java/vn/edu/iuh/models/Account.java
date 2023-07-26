8bb35890ba069290f56019ebc84a80da258ea760
Nguyễn Minh Quân <20098151.quan@student.iuh.edu.vn>
2023-07-26 14:51:14 +0700

    init project

---
+++ spring_boot_microservices/register-service/src/main/java/vn/edu/iuh/models/Account.java
@@ -0,0 +1,19 @@
+package vn.edu.iuh.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "accounts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
---
+++ spring_boot_microservices/register-service/src/main/java/vn/edu/iuh/repositories/AccountRepository.java
@@ -0,0 +1,4 @@
+package vn.edu.iuh.repositories;
+
+public interface AccountRepository {
+}
